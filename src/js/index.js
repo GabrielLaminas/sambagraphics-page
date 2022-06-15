4d80fa8a1eb1d68c980188f8056d7725d8921581
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-06-15 13:20:26 -0300

    adicionando responsividade no header
    Foi adicionado responsividade no header da pagina.
    Foi adicionado javascript para pegar o evento de click para mudar os icones, como também evento de resize para resetar alguns eventos.

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+const buttonMenu 
+  = document.querySelector('.show--button');
+
+const listShow 
+  = document.querySelector('.header__nav');
+
+const search 
+  = document.querySelector('.header__search');
+
+const flexContainer 
+  = document.querySelector('.container__flex');
+
+
+buttonMenu.addEventListener('click', function({target}){
+  target.classList.toggle('active');
+  listShow.classList.toggle('active');
+
+  if(target.getAttribute('class') === 'show--button active'){
+    target.style.background 
+      = 'url("../../../assets/images/iconx.png")'
+  }
+
+  if(target.getAttribute('class') === 'show--button'){
+    target.style.background 
+      = 'url("../../../assets/images/menu.png")'
+  }
+})
+
+window.addEventListener('resize', () => {
+  if(this.innerWidth <= 500){
+    listShow.append(search);
+  }
+  else if(this.innerWidth >= 1280){
+    buttonMenu.classList.remove('active');
+    buttonMenu.style.background 
+      = 'url("../../../assets/images/menu.png")';
+    listShow.classList.remove('active');
+  } 
+  else{
+    flexContainer.append(search);
+  }
+});
